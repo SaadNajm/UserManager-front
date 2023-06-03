df1925f5b7e30d663409ff2c1b55c214c18056fa
Saad Najm <saadnajm.msc@gmail.com>
2023-06-03 21:56:43 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { UpdateuserComponent } from './components/updateuser/updateuser.component';
+import { ViewusersComponent } from './components/viewusers/viewusers.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdduserComponent,
+    UpdateuserComponent,
+    ViewusersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
